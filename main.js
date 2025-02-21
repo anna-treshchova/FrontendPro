596afd399c731d922c14ce0c9cacdf3ecb4cb3c3
Anna Treshchova <anna.treshchova@gmail.com>
2025-02-21 10:14:34 +0200

    hw3.3

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+let number = 10369;
 
+// let d1 = (number / 10000) - (number % 1000 / 10000); - doesn't work (result is 0.9999999999999999)
+let d1 = ((number / 100000) - (number % 10000 / 100000)) * 10;
+let d2 = (number % 10000 /1000) - (number % 1000 / 1000);
+let d3 = (number % 1000 / 100) - (number % 100 / 100);
+let d4= (number % 100 / 10) - (number % 10 / 10);
+let d5 = number % 10;
+
+console.log(`${d1} ${d2} ${d3} ${d4} ${d5}`);
