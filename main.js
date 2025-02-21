a5f51294f7f3da8fd4f071f86651101f7e88599a
Anna Treshchova <anna.treshchova@gmail.com>
2025-02-21 10:08:54 +0200

    hw3.1

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+let name = "Anna";
+console.log(typeof(name));
 
+let age = 23;
+console.log(typeof(age));
+
+let isLoggedIn = true;
+console.log(typeof(isLoggedIn));
+
+let bigNumber = 9007199254740992n;
+console.log(typeof(bigNumber));
