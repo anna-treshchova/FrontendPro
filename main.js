ddd5528edfeb4291557c3ed6023c855a0ebf6fb9
Anna Treshchova <anna.treshchova@gmail.com>
2025-02-27 12:54:19 +0200

    hw5.4

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+let n = Number(prompt("Enter your number"));
 
+let isPrime = true;
+
+if (n && Number.isInteger(n) && n > 1) {
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0) {
+            console.log(`Your number ${n} is a composite number because it's divisible by ${i}`);
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime) {
+        console.log(`Your number ${n} is a prime number.`);
+    }
+} else {
+    console.log("Invalid input. Please enter an integer greater than 1.");
+}
