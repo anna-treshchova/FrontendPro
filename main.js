eacc83c5bc111b834ce523e2411e007136411214
Anna Treshchova <anna.treshchova@gmail.com>
2025-03-03 18:46:42 +0200

    hw6.3

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+function removeElement(array, item) {
+    const index = array.indexOf(item);
+
+    index !== -1 ?  array.splice(index, 1) : console.log(`The element ${item} is already missing from the list`);
+}
+
+const array = [1, 3, 4, 6, 2, 5, 7];
+removeElement(array,4);
+
+console.log(array);
 
