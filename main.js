4de6148fb549c6c55f1180d95e08d37e427749f9
Anna Treshchova <anna.treshchova@gmail.com>
2025-03-03 18:43:40 +0200

    hw6.2

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+function average(arr) {
+    let result = 0;
+    let amount = 0;
 
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === "number") {
+            result += arr[i];
+            amount += 1
+        }
+    }
+
+    return amount !== 0 ? result / amount : "No numbers found in the array";
+}
+
+console.log(average([10, 20, true, 30, "Anna", 40, 10, false]));
