a11688816bfa2cd270b63c9662dba1cf398083b9
Anna Treshchova <anna.treshchova@gmail.com>
2025-03-03 18:40:08 +0200

    hw6.1

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+function removeCharacters(str, arrToDelete) {
+
+   let result = ""
+    for(let i = 0; i < str.length; i++) {
+        if(!arrToDelete.includes(str[i])) {
+            result += str[i]
+        }
+    }
+    console.log(result)
+}
+
+removeCharacters("hello world", ["l", "d"]);
 
