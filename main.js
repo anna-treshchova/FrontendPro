0530ad9766cd5a0b91746f822f897a53e7d0ec31
Anna Treshchova <anna.treshchova@gmail.com>
2025-03-13 06:28:52 +0200

    hw7.1

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+function createSum () {
+    let sum = 0;
+    return function (number) {
+        return sum += number;
+    }
+}
 
+let sum = createSum();
+
+console.log(sum(4));
+console.log(sum(6));
+console.log(sum(10));
+console.log(sum(7));
