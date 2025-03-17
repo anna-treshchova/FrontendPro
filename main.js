8aca79334555e18d43cf8f8e8cfff386a39ba151
Anna Treshchova <anna.treshchova@gmail.com>
2025-03-17 15:38:04 +0200

    hw9.1

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
+const company = {
+    sales: [
+        {name: 'John', salary: 1000},
+        {name: 'Alice', salary: 600}
+    ],
+    development: {
+        web: [
+            {name: 'Peter', salary: 2000},
+            {name: 'Alex', salary: 1800},
+        ],
+        internals: [
+            {name: 'Jack', salary: 1300},
+        ]
+    }
+}
 
+
+function countSalaries(department) {
+    if(Array.isArray(department)) {
+        return department.reduce((acc, employee) => acc + employee.salary, 0);
+    }
+
+    return Object.values(department).reduce((acc, subDep) => acc + countSalaries(subDep), 0);
+}
+console.log(`The sum of employees' salaries is $${countSalaries(company)}.`);
