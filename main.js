801420920ffe67ecba40f4eb4705fe218c2c3162
Anna Treshchova <anna.treshchova@gmail.com>
2025-03-21 08:38:20 +0200

    hw10.1

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+const user = {
+    name: "John Doe",
+    age: 30,
+    email: "john@doe.com",
+    city: "New York",
+    showData() {
+        console.log(`Name: ${this.name}`)
+        console.log(`Age: ${this.age}`)
+        console.log(`Email: ${this.email}`)
+        console.log(`City: ${this.city}`)
+    }
+}
 
+user.showData()
