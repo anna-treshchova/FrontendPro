b4eb62348ef96c782ee99bf5f477b8ac562a00bc
Anna Treshchova <anna.treshchova@gmail.com>
2025-03-30 12:53:51 +0300

    hw11.1

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
+const addPythTable = document.querySelector("#pythBtn")
+const pythTableContainer = document.querySelector("#pythTableContainer")
 
+function createPythTable(size) {
+    pythTableContainer.innerHTML = "";
+
+    const pythTable = document.createElement("table")
+    pythTable.classList.add("pythTable")
+
+    for (let i = 0; i <= size; i++) {
+        let row = document.createElement("tr")
+
+        for (let j = 0; j <= size; j++) {
+            let cell = document.createElement("td")
+            cell.classList.add("pythCell")
+
+            if (i === 0 && j === 0) {
+                cell.textContent = "*";
+                cell.classList.add("cellGray");
+            } else if (i === 0 || j === 0) {
+                cell.textContent = i || j;
+                cell.classList.add("cellGray");
+            } else {
+                cell.textContent = j * i;
+            }
+            row.appendChild(cell)
+        }
+        pythTable.appendChild(row)
+    }
+    pythTableContainer.appendChild(pythTable)
+}
+
+addPythTable.addEventListener("click", () => {
+    createPythTable(10)
+})
