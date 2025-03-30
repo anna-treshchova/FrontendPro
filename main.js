f2438fd777aa6fdcf8b8b40b4d529f985c47da22
Anna Treshchova <anna.treshchova@gmail.com>
2025-03-30 13:31:32 +0300

    hw11.2

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+const title = document.querySelector('.title');
+const crossedText = document.querySelector('.crossed-text');
+const btn = document.querySelector('.btn');
 
+btn.addEventListener('click', () => {
+    title.classList.toggle('title_active');
+    btn.classList.toggle('btn_active');
+    crossedText.classList.toggle('crossed-text_active');
+})
