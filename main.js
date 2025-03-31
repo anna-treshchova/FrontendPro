db903e1af5ea10f96a91f212124f7f5847df3751
Anna Treshchova <anna.treshchova@gmail.com>
2025-03-31 13:28:57 +0300

    hw11.3

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+const img = document.querySelector("#img");
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", function () {
+    let random = Math.floor(Math.random() * 9) + 1;
+    img.src = `./img/${random}.jpg`;
+})
+
 
