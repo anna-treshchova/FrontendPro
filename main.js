a4c9d6c028988f169ad44f9a6b40ccb2480f2a12
Anna Treshchova <anna.treshchova@gmail.com>
2025-04-02 13:01:06 +0300

    hw12.1

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+const btn = document.querySelector('#enter-btn');
+const link = document.querySelector('#follow-link');
 
+btn.addEventListener('click', () => {
+    let promptLink = prompt("Enter your link here").trim();
+    if (!promptLink) {
+        alert('Invalid URL');
+        link.href = "#";
+        return;
+    }
+
+    if (!promptLink.startsWith('https://') && !promptLink.startsWith('http://')) {
+        promptLink = "https://" + promptLink;
+    }
+
+    link.href = promptLink;
+    btn.classList.add('btn_disabled');
+    link.classList.remove('btn_disabled');
+})
