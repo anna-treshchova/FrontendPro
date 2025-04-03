cac0c001e05b19b5ab0b750f9c574065c90222cc
Anna Treshchova <anna.treshchova@gmail.com>
2025-04-03 11:53:38 +0300

    hw12.3

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+const taskList = document.querySelector("#task-list");
+const taskInput = document.querySelector("#task-input");
+const taskBtn = document.querySelector("#task-btn");
 
+taskList.addEventListener("click", (e) => {
+    if (e.target.classList.contains("delete-btn")) {
+        e.target.parentElement.remove();
+    }
+});
+
+
+taskBtn.addEventListener("click", () => {
+    const taskName = taskInput.value.trim();
+
+    if (taskName) {
+        const li = document.createElement("li");
+        li.innerHTML = `${taskName} <button class="delete-btn">Delete</button>`;
+        taskList.appendChild(li);
+    }
+    taskInput.value = "";
+})
