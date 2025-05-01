c6089763253570b6ee3f2e4d675052316e41cd5e
Anna Treshchova <anna.treshchova@gmail.com>
2025-05-01 17:36:24 +0300

    Create ToDo list functionality

--- main.js
+++ main.js
@@ -779,7 +779,9 @@ function getCookie(cookieKey) {
     SessionStorage        До закриття вкладки                                5-10MB
     LocalStorage          Безстроково                                        5-10MB
     Cookies               До заданої дати або до закриття браузера           4KB
-----------------------------------------------------------------------------------------------------------------------*/
+------------------------------------------------------------------------------------------------------------------------
+
+TASK: Create Movie List functionality
 
 "use strict"
 
@@ -946,5 +948,169 @@ renderMovies() — це перший поштовх, й він взаємоді�
 
  5. Колізія id у HTML
     Елемент id має бути унікальним. Якщо є два елементи з однаковим id, поведінка стає непередбачуваною
+
+----------------------------------------------------------------------------------------------------------------------*/
+
+// TASK: Create ToDo List functionality
+
+"use strict"
+
+const formInput = document.querySelector(".js-form__input");
+const addBtn = document.querySelector(".js-add-btn");
+const toDoList = document.querySelector(".js-todo__list");
+
+let tasks = JSON.parse(localStorage.getItem("toDoItems")) || [];
+
+addBtn.addEventListener ("click", (e) => {
+    e.preventDefault();
+
+    if (!formInput.value.trim()) {
+        return
+    }
+
+    tasks.push({
+        id: crypto.randomUUID(),
+        description: formInput.value.trim(),
+        completed: false
+    });
+
+    localStorage.setItem("toDoItems", JSON.stringify(tasks));
+
+    renderToDoList();
+
+    formInput.value = "";
+});
+
+toDoList.addEventListener("click", (e) => {
+    handleDelete(e);
+    handleCheckbox(e);
+});
+
+function handleDelete(e) {
+    if (e.target.classList.contains("delete-btn")) {
+        tasks = tasks.filter(task => task.id !== e.target.dataset.id);
+        localStorage.setItem("toDoItems", JSON.stringify(tasks));
+
+        renderToDoList();
+    }
+}
+
+function handleCheckbox(e) {
+    if (e.target.classList.contains("checkbox")) {
+        const id = e.target.dataset.id;
+        const task = tasks.find(task => task.id === id);
+
+        if (task) {
+            task.completed = e.target.checked;
+        }
+
+        const listItem = e.target.closest("li");
+        listItem.querySelector("span").classList.toggle("completed", task.completed) // візуально показуємо, що завдання виконано
+
+        localStorage.setItem("toDoItems", JSON.stringify(tasks)); //оновлюємо localStorage, щоб зберегти новий стан task.completed
+    }
+}
+
+function renderToDoList () {
+    toDoList.innerHTML = "";
+
+    tasks.forEach(task  => {
+        const toDoItem = document.createElement("li");
+        toDoItem.classList.add("todo__item");
+
+        toDoItem.innerHTML = `
+        <label>
+            <input class="checkbox" type="checkbox" data-id="${task.id}" ${task.completed ? "checked" : ""}>
+        </label>
+        <span class="todo__item-description ${task.completed ? "completed" : ""}" >${task.description}</span>
+        <button class="delete-btn" data-id="${task.id}">Delete</button>
+    `;
+        toDoList.appendChild(toDoItem);
+    })
+}
+
+renderToDoList();
+
+
+/*---------------
+
+ e.target.checked — це властивість елемента <input type="checkbox">, яка автоматично змінюється в залежності від того,
+ чи була поставлена галочка.
+
+ e.target — це сам елемент, на якому стався клік (у нашому випадку це input з типом checkbox).
+
+ Тобто коли користувач ставить чи знімає галочку, властивість checked оновлюється автоматично на булеве значення, яке ми
+ можемо зчитувати та використовувати
+
+----------------
+
+ ${task.completed ? "checked" : "" нам необхідно, БО:
+
+ 1. DOM (візуальні зміни на сторінці):   listItem.querySelector("span").classList.toggle("completed", task.completed)
+
+   Цей рядок лише додає або прибирає клас "completed" у HTML для стилізації (text-decoration: line-through тощо).
+
+  ❗Але DOM НЕ ЗБЕРІГАЄТЬСЯ ПІСЛЯ ПЕРЕЗАВАНТАЖЕННЯ СТОРІНКИ — браузер повністю перебудовує сторінку, тому всі ці стилі
+    зникають.
+
+
+  2. localStorage (збереження стану):   localStorage.setItem("toDoItems", JSON.stringify(tasks));
+
+     Це — збереження даних, а не вигляду.
+
+     Саме тут ми зберігаємо: task.completed = true/false
+
+     А потім при перезавантаженні отримуємо збережені дані (completed: true/false) і в renderToDoList() використовуємо
+     {task.completed ? "checked" : ""}  щоб відновити вигляд — галочку і перекреслений текст
+
+
+ВИСНОВОК:
+   ◦ task.completed = true — зберігає стан у даних
+
+   ◦ localStorage.setItem(...) — записує це у пам'ять браузера
+
+   ◦ ${task.completed ? "checked" : ""} — додає галочку та стилі до тексту завдання при рендері після перезавантаження
+
+   ◦ .classList.toggle(...) — змінює вигляд одразу при кліку, щоб користувач бачив зміни без оновлення
+
+------------------------------------------------------------------------------------------------------------------------
+
+              КОЛЕКЦІЇ, ЯКІ СХОЖІ НА МАСИВИ, АЛЕ НЕ МАЮТЬ ДОСТУПУ ДО СТАНДАРТНИХ МЕТОДІВ МАСИВІВ:
+
+
+ HTMLCollection
+-----------------
+
+ Це спеціальна колекція, яка виглядає схоже на масив, але не є справжнім масивом.
+
+ Тобто ми не можемо використовувати методи масивів forEach(), map(), filter(), reduce(), find(), indexOf() без
+ перетворення цієї колекції в масив за допомогою Array.from()
+
+ ЩО ПОВЕРТАЄ HTMLCollection:
+
+    1. element.children   (повертає дочірні елементи)
+
+    2. document.getElementsByTagName(tagName)
+    3. document.getElementsByClassName(className)
+
+    4. element.getElementsByTagName(tagName)     (те саме, що й 2, але в межах конкретного елемента)
+    5. element.getElementsByClassName(className)
+
+    6. document.forms     (повертає всі <form> у документі)
+    7. document.images
+    8. document.links     (повертає всі <a> з href у документі)
+
+    9. form.elements      (повертає всі елементи форми)
+
+❗HTMLCollection повертає живу колекцію (live collection), яка автоматично оновлюється при зміні DOM-елементів ❗
+
+
+   NodeList
+--------------
+
+ Це також спеціальна колекція, яка виглядає схоже на масив, але не є справжнім масивом (псевдомасив).
+ Найпоширеніший спосіб, який повертає NodeList - це document.querySelectorAll(selector).
+
+❗Він повертає статичний NodeList, який не оновлюється автоматично при зміні DOM-елементів ❗
 
 ----------------------------------------------------------------------------------------------------------------------*/
