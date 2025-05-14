0e466613b4c323b3be41cacdb53aa6fb9d83e11c
Anna Treshchova <anna.treshchova@gmail.com>
2025-05-14 07:15:44 +0300

    add static Calculator class

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
+class Calculator {
+    static add(a, b) {
+        return a + b;
+    }
+    static subtract(a, b) {
+        return a - b;
+    }
+    static multiply(a, b) {
+        return a * b;
+    }
+    static divide(a, b) {
+        return a / b;
+    }
+}
+
+console.log(Calculator.add(5, 3));
+console.log(Calculator.subtract(10, 4));
+console.log(Calculator.multiply(3, 6));
+console.log(Calculator.divide(8, 2));
 
